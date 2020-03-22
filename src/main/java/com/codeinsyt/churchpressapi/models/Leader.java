77c410e60927bd3ac74beab70f4ae4b7c636b6cc
kwamekert <tomriddle477@gmail.com>
2020-03-22 14:28:38 +0000

    Leader model completed

--- src/main/java/com/codeinsyt/churchpressapi/models/Leader.java
+++ src/main/java/com/codeinsyt/churchpressapi/models/Leader.java
@@ -10,5 +10,57 @@ public class Leader {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @Lob
+    private String description;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="department_id", nullable= false)
+    private Department department;
+
+    public Leader() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Leader{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", department=" + department +
+                '}';
+    }
 }
