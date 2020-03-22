71ccefd69e85db0b6831d75933b877068c0bbb44
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:25:36 +0000

    Sermon service implemented

--- src/main/java/com/codeinsyt/churchpressapi/services/impl/SermonServiceImpl.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/impl/SermonServiceImpl.java
@@ -1,4 +1,50 @@
 package com.codeinsyt.churchpressapi.services.impl;
 
-public class SermonServiceImpl {
+import com.codeinsyt.churchpressapi.models.Sermon;
+import com.codeinsyt.churchpressapi.services.interfaces.SermonService;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+
+public class SermonServiceImpl implements SermonService {
+
+
+    public HashMap<String, Object> responseAPI(Object data, String message, HttpStatus status){
+        HashMap<String, Object> responseData = new HashMap<>();
+        responseData.put("data",data);
+        responseData.put("message",message);
+        responseData.put("status", status.value());
+
+        return responseData;
+    }
+
+    @Override
+    public HashMap<String, Object> createSermon(Sermon sermon) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> updateSermon(Sermon sermon) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> softDeleteSermon(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> hardDeleteSermon(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> listSermons() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getSermon(Long id) {
+        return null;
+    }
 }
--- src/main/java/com/codeinsyt/churchpressapi/services/impl/UserServiceImpl.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/impl/UserServiceImpl.java
@@ -1,4 +1,53 @@
 package com.codeinsyt.churchpressapi.services.impl;
 
-public class UserServiceImpl {
+import com.codeinsyt.churchpressapi.models.User;
+import com.codeinsyt.churchpressapi.services.interfaces.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    public HashMap<String, Object> responseAPI(Object data, String message, HttpStatus status){
+        HashMap<String, Object> responseData = new HashMap<>();
+        responseData.put("data",data);
+        responseData.put("message",message);
+        responseData.put("status", status.value());
+
+        return responseData;
+    }
+
+
+    @Override
+    public HashMap<String, Object> createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> updateUser(User user) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> softDeleteUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> hardDeleteUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> listUsers() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getUser(Long id) {
+        return null;
+    }
 }
