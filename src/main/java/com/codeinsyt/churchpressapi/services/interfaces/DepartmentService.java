d8c9c1e059b396ec282cc702129282b17a82568f
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:15:28 +0000

    Department Service updated

--- src/main/java/com/codeinsyt/churchpressapi/services/interfaces/DepartmentService.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/interfaces/DepartmentService.java
@@ -1,4 +1,20 @@
 package com.codeinsyt.churchpressapi.services.interfaces;
 
+import com.codeinsyt.churchpressapi.models.Department;
+
+import java.util.HashMap;
+
 public interface DepartmentService {
+
+    HashMap<String, Object> createDepartment(Department department);
+
+    HashMap<String, Object> updateDepartment(Department department);
+
+    HashMap<String, Object>  softDeleteDepartment(Long id);
+
+    HashMap<String, Object> hardDeleteDepartment(Long id);
+
+    HashMap<String, Object> listDepartments();
+
+    HashMap<String, Object> getDepartment(Long id);
 }
--- src/main/java/com/codeinsyt/churchpressapi/services/interfaces/LeaderService.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/interfaces/LeaderService.java
@@ -1,4 +1,20 @@
 package com.codeinsyt.churchpressapi.services.interfaces;
 
+import com.codeinsyt.churchpressapi.models.Leader;
+
+import java.util.HashMap;
+
 public interface LeaderService {
+
+    HashMap<String, Object> createLeader(Leader leader);
+
+    HashMap<String, Object> updateLeader(Leader leader);
+
+    HashMap<String, Object>  softDeleteLeader(Long id);
+
+    HashMap<String, Object> hardDeleteLeader(Long id);
+
+    HashMap<String, Object> listLeaders();
+
+    HashMap<String, Object> getLeader(Long id);
 }
