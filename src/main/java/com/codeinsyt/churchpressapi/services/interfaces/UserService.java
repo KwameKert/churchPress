f21cd41bc4279a27e93bb52c3921625f31c90353
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:22:21 +0000

    User service updated successfully

--- src/main/java/com/codeinsyt/churchpressapi/services/interfaces/UserService.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/interfaces/UserService.java
@@ -1,4 +1,21 @@
 package com.codeinsyt.churchpressapi.services.interfaces;
 
+import com.codeinsyt.churchpressapi.models.User;
+
+import java.util.HashMap;
+
 public interface UserService {
+
+
+    HashMap<String, Object> createUser(User user);
+
+    HashMap<String, Object> updateUser(User user);
+
+    HashMap<String, Object>  softDeleteUser(Long id);
+
+    HashMap<String, Object> hardDeleteUser(Long id);
+
+    HashMap<String, Object> listUsers();
+
+    HashMap<String, Object> getUser(Long id);
 }
