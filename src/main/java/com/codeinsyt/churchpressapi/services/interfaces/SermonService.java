bcb54599c2a6f441354a8ccd02cbd710cbaba485
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:20:42 +0000

    Sermon service updated successfully

--- src/main/java/com/codeinsyt/churchpressapi/services/interfaces/SermonService.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/interfaces/SermonService.java
@@ -1,4 +1,22 @@
 package com.codeinsyt.churchpressapi.services.interfaces;
 
+import com.codeinsyt.churchpressapi.models.Sermon;
+
+import java.util.HashMap;
+
 public interface SermonService {
+
+
+    HashMap<String, Object> createSermon(Sermon sermon);
+
+    HashMap<String, Object> updateSermon(Sermon sermon);
+
+    HashMap<String, Object>  softDeleteSermon(Long id);
+
+    HashMap<String, Object> hardDeleteSermon(Long id);
+
+    HashMap<String, Object> listSermons();
+
+    HashMap<String, Object> getSermon(Long id);
+
 }
