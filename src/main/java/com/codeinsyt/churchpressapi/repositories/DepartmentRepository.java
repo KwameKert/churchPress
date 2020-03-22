e3b15ea2590c78787429cb4d25646edf0a5b9c36
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:01:26 +0000

    Department repository updated

--- src/main/java/com/codeinsyt/churchpressapi/repositories/DepartmentRepository.java
+++ src/main/java/com/codeinsyt/churchpressapi/repositories/DepartmentRepository.java
@@ -1,4 +1,19 @@
 package com.codeinsyt.churchpressapi.repositories;
 
-public interface DepartmentRepository {
+import com.codeinsyt.churchpressapi.models.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface DepartmentRepository  extends JpaRepository<Department, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Department d SET d.status = ?2 WHERE s.id = ?1")
+    @Transactional
+    int UpdateStudentStatus(Long departmentId, String status);
+
+    List<Department> findAllByStatusOrderByIdAsc(String status);
 }
