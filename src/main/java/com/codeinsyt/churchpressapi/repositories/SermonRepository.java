c2d585909c0a8c4fafef9c8a75f3629fb91dd8a3
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:07:03 +0000

    Sermon repositor updated

--- src/main/java/com/codeinsyt/churchpressapi/repositories/SermonRepository.java
+++ src/main/java/com/codeinsyt/churchpressapi/repositories/SermonRepository.java
@@ -1,4 +1,19 @@
 package com.codeinsyt.churchpressapi.repositories;
 
-public interface SermonRepository {
+import com.codeinsyt.churchpressapi.models.Sermon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface SermonRepository extends JpaRepository<SermonRepository, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Sermon s SET s.status = ?2 WHERE s.id = ?1")
+    @Transactional
+    int UpdateSermonStatus(Long studentId, String status);
+
+    List<Sermon> findAllByStatusOrderByIdAsc(String status);
 }
