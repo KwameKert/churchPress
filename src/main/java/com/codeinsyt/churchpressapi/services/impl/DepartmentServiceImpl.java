cdb1bae96e10ef276a6a8e4cc2c34be8de04cee5
kwamekert <tomriddle477@gmail.com>
2020-03-22 15:27:25 +0000

    department service implemented successfully

--- src/main/java/com/codeinsyt/churchpressapi/services/impl/DepartmentServiceImpl.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/impl/DepartmentServiceImpl.java
@@ -1,4 +1,52 @@
 package com.codeinsyt.churchpressapi.services.impl;
 
-public class DepartmentServiceImpl {
+
+import com.codeinsyt.churchpressapi.models.Department;
+import com.codeinsyt.churchpressapi.services.interfaces.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class DepartmentServiceImpl  implements DepartmentService {
+
+    public HashMap<String, Object> responseAPI(Object data, String message, HttpStatus status){
+        HashMap<String, Object> responseData = new HashMap<>();
+        responseData.put("data",data);
+        responseData.put("message",message);
+        responseData.put("status", status.value());
+
+        return responseData;
+    }
+
+    @Override
+    public HashMap<String, Object> createDepartment(Department department) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> updateDepartment(Department department) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> softDeleteDepartment(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> hardDeleteDepartment(Long id) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> listDepartments() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getDepartment(Long id) {
+        return null;
+    }
 }
