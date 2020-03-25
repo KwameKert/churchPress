c58f74fb53a4085e6d12a1c3a3c80f71514d9006
kwamekert <tomriddle477@gmail.com>
2020-03-25 20:37:19 +0000

    working on user controller

--- src/main/java/com/codeinsyt/churchpressapi/controllers/UserController.java
+++ src/main/java/com/codeinsyt/churchpressapi/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.codeinsyt.churchpressapi.controllers;
 
+import com.codeinsyt.churchpressapi.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
 public class UserController {
+
+    public UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
