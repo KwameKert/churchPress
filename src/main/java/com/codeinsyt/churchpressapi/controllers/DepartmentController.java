f67e3d651a90064d9a1862e9843f608ab7574a00
kwamekert <tomriddle477@gmail.com>
2020-03-31 01:00:49 +0000

    Create department endpoint created

--- src/main/java/com/codeinsyt/churchpressapi/controllers/DepartmentController.java
+++ src/main/java/com/codeinsyt/churchpressapi/controllers/DepartmentController.java
@@ -1,4 +1,30 @@
 package com.codeinsyt.churchpressapi.controllers;
 
+import com.codeinsyt.churchpressapi.models.Department;
+import com.codeinsyt.churchpressapi.services.interfaces.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("api/v1/department/")
 public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+
+    public ResponseEntity<?> addDepartment(@Valid @RequestBody Department department){
+        return new ResponseEntity<>(this.departmentService.createDepartment(department), HttpStatus.OK);
+    }
 }
