d83680edce42b470847790b28742f5553cd4ae97
kwamkert <tomriddle477@gmail.com>
2020-04-06 00:28:02 +0000

    DTO ADDED

---
+++ src/main/java/com/codeinsyt/churchpressapi/dto/LeaderDTO.java
@@ -0,0 +1,66 @@
+package com.codeinsyt.churchpressapi.dto;
+
+import com.codeinsyt.churchpressapi.models.Department;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class LeaderDTO {
+
+
+    private String name;
+    @Lob
+    private String description;
+    private String stat;
+    private String image_url;
+    private Long department_id;
+
+
+    public LeaderDTO() {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public Long getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(Long department_id) {
+        this.department_id = department_id;
+    }
+}
+
