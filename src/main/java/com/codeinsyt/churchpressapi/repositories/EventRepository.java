839ed697d8428ff4915661248221db3c095855ed
kwamkert <tomriddle477@gmail.com>
2020-04-10 20:05:19 +0000

    update queries added to event Repository

--- src/main/java/com/codeinsyt/churchpressapi/repositories/EventRepository.java
+++ src/main/java/com/codeinsyt/churchpressapi/repositories/EventRepository.java
@@ -2,10 +2,18 @@ package com.codeinsyt.churchpressapi.repositories;
 
 import com.codeinsyt.churchpressapi.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Event e SET e.stat = ?2 WHERE e.id = ?1")
+    @Transactional
+    int UpdateEventStat(Long eventId, String status);
 }
