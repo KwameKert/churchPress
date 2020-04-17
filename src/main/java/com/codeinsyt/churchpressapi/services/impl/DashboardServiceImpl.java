02bad13ca248f437c60423fc3439824191a6e286
kwamkert <tomriddle477@gmail.com>
2020-04-17 20:16:42 +0000

    Dashboard service implemented

--- src/main/java/com/codeinsyt/churchpressapi/services/impl/DashboardServiceImpl.java
+++ src/main/java/com/codeinsyt/churchpressapi/services/impl/DashboardServiceImpl.java
@@ -3,6 +3,8 @@ package com.codeinsyt.churchpressapi.services.impl;
 import com.codeinsyt.churchpressapi.models.Event;
 import com.codeinsyt.churchpressapi.services.interfaces.DashboardService;
 
+import java.util.HashMap;
+
 public class DashboardServiceImpl implements DashboardService {
 
     @Override
@@ -17,6 +19,11 @@ public class DashboardServiceImpl implements DashboardService {
 
     @Override
     public Event getNextEvent() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> getDashboardComponents() {
         return null;
     }
 }
